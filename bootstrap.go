72a8b0ebf79089c8d3969eb0ad0b1e94412aa5aa
DoniLite <yaomessan13@outlook.com>
2026-05-27 14:59:24 +0000

    chore: implement the bootstrap logic

--- bootstrap.go
+++ bootstrap.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"path"
 
 	"github.com/Arnel7/kappelas-sdk-go"
 	"github.com/DoniLite/kapelas-bot/conf"
+	"github.com/DoniLite/kapelas-bot/handlers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -13,13 +15,14 @@ import (
 )
 
 var (
-	bot  *kappelas.Bot
-	user *kappelas.User
+	bot    *kappelas.Bot
+	user   *kappelas.User
 	router *gin.Engine
 )
 
 func Bootstrap() {
 	cwd, err := os.Getwd()
+	ctx := context.Background()
 	if err != nil {
 		log.Fatal("Error getting current working directory, make sure to run the bot from the project root directory")
 	}
@@ -35,12 +38,29 @@ func Bootstrap() {
 	bot = kappelas.NewBot(env.GetString(conf.BOT_TOKEN))
 	user = kappelas.NewUser(env.GetString(conf.BOT_PLATFORM_API_KEY))
 	router = gin.Default()
-}
 
-func debugLoader() {
+	bot.Webhooks.Set(ctx, kappelas.SetWebhookParams{
+		URL: env.GetWebHookBotURL(),
+	})
+	user.Webhooks.Set(ctx, kappelas.SetWebhookParams{
+		URL: env.GetWebHookUserURL(),
+	})
+	log.Printf("Bot webhook set to: %s", env.GetWebHookBotURL())
+	log.Printf("User webhook set to: %s", env.GetWebHookUserURL())
 
-}
+	router = handlers.BuildRouter(&handlers.RouterDeps{
+		Router: router,
+		Bot:    bot,
+		User:   user,
+	})
 
-func prodLoader() {
-
+	isDev := env.GetBool(conf.BOT_IS_DEVELOPMENT)
+	if isDev {
+		log.Printf("Running in dev mode, starting the channel to listen for bot updates...")
+		go func() {
+			bot.Start()
+			select {}
+		}()
+	}
+	router.Run(":" + env.GetString(conf.SERVER_PORT))
 }
